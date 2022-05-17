7f52592ea3526629684a2c1805a61e81d290222b
JuHyeon-An <ajh2042@naver.com>
2022-05-17 14:28:36 +0900

    [프로그래머스] 내적

--- Get-Dot-Product/GetDotProduct.java
+++ Get-Dot-Product/GetDotProduct.java
@@ -1,3 +1,9 @@
+/* ==============================================================
+ *	[월간 코드 챌린지 시즌1] 내적
+ *  https://programmers.co.kr/learn/courses/30/lessons/70128
+ * ==============================================================
+ */
+
 class GetDotProduct{
     public int solution(int[] a, int[] b) {
         int answer = 0;
