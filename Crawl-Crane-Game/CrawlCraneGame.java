b980cc8b5fa25df7a1a24b70bb726706a68fd81d
JuHyeon-An <ajh2042@naver.com>
2022-05-17 14:27:16 +0900

    [프로그래머스] 크레인 인형뽑기 게임

--- Crawl-Crane-Game/CrawlCraneGame.java
+++ Crawl-Crane-Game/CrawlCraneGame.java
@@ -1,5 +1,10 @@
 import java.util.Stack;
 
+/* ==============================================================
+ *	[2019 카카오 개발자 겨울 인턴십] 크레인 인형뽑기 게임
+ *  https://programmers.co.kr/learn/courses/30/lessons/64061
+ * ==============================================================
+ */
 class CrawlCraneGame{
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
