869f2b3e85e168cb209ec8fcef4d7e0d3b1dca2d
JuHyeon An <57624327+JuHyeon-An@users.noreply.github.com>
2022-05-26 21:53:54 +0900

    [프로그래머스] K번째 수

--- Kth-Number/KthNumber.java
+++ Kth-Number/KthNumber.java
@@ -1,5 +1,11 @@
 import java.util.Arrays;
 
+/* ==============================================================
+ *	[정렬] K번째 수
+ *  https://programmers.co.kr/learn/courses/30/lessons/42748
+ * ==============================================================
+ */
+
 public class KthNumber {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[3];
