deff0fc40f9c231d58d662b19d666858678acc01
JuHyeon-An <ajh2042@naver.com>
2022-06-02 17:22:06 +0900

    [프로그래머스] 모의고사

--- Mock-Test/MockTest.java
+++ Mock-Test/MockTest.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
+/* ==============================================================
+ *	[완전탐색] 모의고사
+ *  https://programmers.co.kr/learn/courses/30/lessons/42840
+ * ==============================================================
+ */
+
 public class MockTest {
 
     public int[] solution(int[] answers) {
