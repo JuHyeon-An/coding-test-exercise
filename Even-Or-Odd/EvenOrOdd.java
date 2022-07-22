6aeb3bb2075959268a67ae3a2ed89f08ad3fd8e9
JuHyeon-An <ajh2042@naver.com>
2022-07-22 15:04:42 +0900

    [프로그래머스] 짝수와 홀수

---
+++ Even-Or-Odd/EvenOrOdd.java
@@ -0,0 +1,14 @@
+/* ==============================================================
+ *	[연습문제] 짝수와 홀수
+ *  https://school.programmers.co.kr/learn/courses/30/lessons/12937
+ * ==============================================================
+ */
+public class EvenOrOdd {
+    public String solution(int num) {
+        return num%2 == 0 ? "Even" : "Odd";
+    }
+
+    public static void main(String[] args) {
+        String answer = new EvenOrOdd().solution(3);
+    }
+}
